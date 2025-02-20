633d54293dd65934f1ae198001b44d6a7d5f0711
Filipp <vasilewskij.fil@gmail.com>
2025-02-20 17:35:55 +0300

    Add get live result

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="aiosofascore",
-    version="0.0.6.1",
+    version="0.0.7.0",
     description="API client for SofaScore soccer data",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
-    author="Philip",  # Ваше имя
+    author="Philip",
     author_email="vasilewskij.fil@gmail.com",
     url="https://github.com/Rooney27/aio_sofascore",
     packages=find_packages(),
